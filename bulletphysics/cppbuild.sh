cceb971711ef4fabc1880b0dc05d07958285ca94
Herbert <hwvenancio@Herbert-PC>
2016-06-12 21:07:31 -0300

    modified cppbuild.sh to build using cygwin+MingW toolchain

--- bulletphysics/cppbuild.sh
+++ bulletphysics/cppbuild.sh
@@ -2,7 +2,7 @@
 # This file is meant to be included by the parent cppbuild.sh script
 if [[ -z "$PLATFORM" ]]; then
     pushd ..
-    bash cppbuild.sh "$@" bullet3
+    bash cppbuild.sh "$@" bulletphysics
     popd
     exit
 fi
@@ -16,6 +16,35 @@ tar -xzvf ../bullet3-$BULLET_VERSION.tar.gz
 cd bullet3-$BULLET_VERSION
 
 case $PLATFORM in
+    cygwin-x86_64)
+        #"$CMAKE" . -G "Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE=../../../mingw-w64-toolchain.cmake \
+        #  -DINSTALL_LIBS=ON -DBUILD_SHARED_LIBS=ON \
+        #  -DINSTALL_LIBS=ON -DBUILD_SHARED_LIBS=ON \
+        #  -DCMAKE_OSX_ARCHITECTURES='i386;x86_64' \
+        #  -DFRAMEWORK=ON \
+        #  -DBUILD_BULLET3=OFF \
+        #  -DBUILD_OPENGL3_DEMOS=OFF \
+        #  -DBUILD_OPENGL3_DEMOS=OFF \
+        #  -DBUILD_CPU_DEMOS=OFF -DBUILD_BULLET2_DEMOS=OFF \
+        #  -DBUILD_CPU_DEMOS=OFF -DBUILD_BULLET2_DEMOS=OFF \
+        #  -DBUILD_EXTRAS=OFF -DBUILD_BULLET3=OFF \
+        #  -DCMAKE_INSTALL_PREFIX=/Library/Frameworks -DCMAKE_INSTALL_NAME_DIR=/Library/Frameworks \
+        #  -DCMAKE_INSTALL_PREFIX=/Library/Frameworks -DCMAKE_INSTALL_NAME_DIR=/Library/Frameworks \
+        #  -DCMAKE_EXE_LINKER_FLAGS="-static-libgcc -static-libstdc++" \
+        #  -DCMAKE_EXE_LINKER_FLAGS="-static-libgcc -static-libstdc++" \
+        #  -DCMAKE_BUILD_TYPE= 
+        cmake . -G "Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE=../../../mingw-w64-toolchain.cmake \
+          -DINSTALL_LIBS=ON -DBUILD_SHARED_LIBS=ON \
+          -DINSTALL_LIBS=ON -DBUILD_SHARED_LIBS=ON \
+          -DCMAKE_OSX_ARCHITECTURES='i386;x86_64' \
+          -DFRAMEWORK=ON \
+          -DBUILD_EXTRAS=OFF -DBUILD_BULLET3=OFF \
+          -DCMAKE_INSTALL_PREFIX=/Library/Frameworks -DCMAKE_INSTALL_NAME_DIR=/Library/Frameworks \
+          -DCMAKE_EXE_LINKER_FLAGS="-static-libgcc -static-libstdc++" \
+          -DCMAKE_BUILD_TYPE= 
+        #cd ../../build/
+        make install
+        ;;
     windows-x86)
         #cd build3
         #SCRIPT_WINPATH='cygpath --windows --absolute "vs2010.bat"'
@@ -25,8 +54,9 @@ case $PLATFORM in
         #cmd //c "vs2010.bat"
         "$CMAKE" . -G "MinGW Makefiles" -DINSTALL_LIBS=ON -DBUILD_SHARED_LIBS=ON \
           -DFRAMEWORK=ON  -DCMAKE_OSX_ARCHITECTURES='i386;x86_64' \
+          -DBUILD_OPENGL3_DEMOS=OFF -DBUILD_CPU_DEMOS=OFF -DBUILD_BULLET2_DEMOS=OFF \
           -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=/Library/Frameworks \
-          -DCMAKE_INSTALL_NAME_DIR=/Library/Frameworks -DBUILD_DEMOS:BOOL=OFF
+          -DCMAKE_INSTALL_NAME_DIR=/Library/Frameworks
         mingw32-make install
         ;;
     windows-x86_64)
